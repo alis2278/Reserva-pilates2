5ee062262382bd69695f8fd57c65eef0a8a6eb58
alis2278 <alicialatorre@gmail.com>
2025-06-24 15:33:35 -0400

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,5 +1,4 @@
 from flask import Flask
-from threading import Thread
 
 app = Flask('')
 
@@ -10,10 +9,3 @@ def home():
 @app.route('/ping')
 def ping():
     return "pong"
-
-def run():
-    app.run(host='0.0.0.0', port=8080)
-
-def keep_alive():
-    t = Thread(target=run)
-    t.start()
